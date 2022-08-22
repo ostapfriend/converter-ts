b86c0da5212b7d54770149a9d65467ce15452dd8
Ostap Salo <jlumon211@gmail.com>
2022-08-22 16:48:44 +0300

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,5 @@
-export function getAllCurrency(country: string = 'USD') {
+export function getAllCurrency(country: string = "USD") {
   const BASE_URL = `https://api.fastforex.io/fetch-all?from=${country}&api_key=ac3294834a-b5dd86afbf-rgv300`;
 
-  return fetch(BASE_URL)
-    .then(response => response.json())
+  return fetch(BASE_URL).then((response) => response.json());
 }
